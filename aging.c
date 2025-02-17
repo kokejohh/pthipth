7edd445e84ed7d11cc7f2a2b9b24f7a6fedde112
Pitchakorn Thiprangsi <kokejohh@gmail.com>
2025-02-17 19:19:57 +0700

    fix: aging together with inheritance

--- aging.c
+++ aging.c
@@ -4,23 +4,23 @@ extern pthipth_private_t *pthipth_prio_head;
 
 extern time_t gettime_ms();
 
-// It's ok but still have bug when doing with priority inheritance
 void pthipth_aging(int aging_factor)
 {
     pthipth_private_t *head_on_prio = pthipth_prio_head;
     pthipth_private_t *tmp = pthipth_prio_head;
 
-    while (1)
+    time_t current_time = gettime_ms();
+    while (tmp)
     {
-	int priority = tmp->priority;
+	int priority = tmp->old_priority;
 	if (priority == LOWEST_PRIORITY) break;
 
-	time_t waiting_time = gettime_ms() - tmp->last_selected;
+	time_t waiting_time = current_time - tmp->last_selected;
 	priority -= (waiting_time / 1000) * aging_factor;
 
 	if (priority < 1) priority = 1;
 
-	//printf("koke %d , calling tid %d, current tid %d waiting_time %ld\n", priority, __pthipth_gettid(), tmp->tid, waiting_time);
+	printf("koke %d , calling tid %d, current tid %d waiting_time %ld\n", priority, __pthipth_gettid(), tmp->tid, waiting_time);
 
 	if (tmp->next) head_on_prio = tmp;
 
@@ -28,8 +28,10 @@ void pthipth_aging(int aging_factor)
 					head_on_prio->next : tmp->inside_next;
 
 	if (priority < tmp->old_priority)
+	    tmp->old_priority = priority;
+	if (priority < tmp->priority)
 	{
-	    tmp->priority = tmp->old_priority = priority;
+	    tmp->priority = priority;
 	    tmp->last_selected = gettime_ms();
 	    pthipth_prio_reinsert(tmp);
 	}

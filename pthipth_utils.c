50bca60773b1a62791ed23e659a62db86661dd6c
Pitchakorn Thiprangsi <kokejohh@gmail.com>
2025-02-24 04:32:59 +0700

    feat: function change to state

--- pthipth_utils.c
+++ pthipth_utils.c
@@ -3,14 +3,17 @@
 #include <sys/time.h>
 
 #include "futex.h"
+#include "pthipth_queue.h"
 #include "pthipth_prio.h"
-
 
 futex_t debug_futex;
 char msg[1000];
 static int debug_futex_init_already = 0;
 
 extern pthipth_private_t *pthipth_prio_head;
+
+extern pthipth_queue_t blocked_state;
+extern pthipth_queue_t sleeping_state;
 
 pid_t __pthipth_gettid()
 {
@@ -46,4 +49,40 @@ void set_time_slice(int ms)
     timer.it_interval.tv_usec = (ms % 1000) * 1000;
 
     setitimer(ITIMER_REAL, &timer, NULL);
+}
+
+void change_to_state(pthipth_private_t *node, int to_state)
+{
+    if (node->state == to_state) return;
+
+    switch (node->state)
+    {
+	case BLOCKED:
+	    pthipth_queue_delete(&blocked_state, node);
+	    break;
+	case SLEEPING:
+	    pthipth_queue_delete(&sleeping_state, node);
+	    break;
+    }
+
+    switch (to_state)
+    {
+	case BLOCKED:
+	    pthipth_prio_delete(node);
+	    pthipth_queue_add(&blocked_state, node);
+	    break;
+	case SLEEPING:
+	    pthipth_prio_delete(node);
+	    pthipth_queue_add(&sleeping_state, node);
+	    break;
+	case READY:
+	    if (node->state == RUNNING) break;
+	    pthipth_prio_insert(node);
+	    break;
+	case DEFUNCT:
+	    pthipth_prio_delete(node);
+	    break;
+	case RUNNING: break;
+    }
+    node->state = to_state;
 }

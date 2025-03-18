f28a67b8bcffe99459b30847dcb873b1b20edea1
Pitchakorn Thiprangsi <kokejohh@gmail.com>
2025-03-19 02:49:54 +0700

    fix: check NULL in barrier and refactor

--- pthipth_barrier.c
+++ pthipth_barrier.c
@@ -5,14 +5,20 @@ extern void change_to_state(pthipth_private_t *node, int state);
 
 extern pthipth_queue_t blocked_state;
 
-void pthipth_barrier_init(pthipth_barrier_t *barrier, int count)
+int pthipth_barrier_init(pthipth_barrier_t *barrier, int count)
 {
+    if (barrier == NULL) return -1;
+
     barrier->count = count;
     atomic_store(&barrier->waiting, 0);
+
+    return 0;
 }
 
 int pthipth_barrier_wait(pthipth_barrier_t *barrier)
 {
+    if (barrier == NULL) return -1;
+
     pthipth_private_t *self = __pthipth_selfptr();
 
     atomic_fetch_add(&barrier->waiting, 1);
@@ -21,19 +27,17 @@ int pthipth_barrier_wait(pthipth_barrier_t *barrier)
 	self->current_barrier = barrier;
 	change_to_state(self, BLOCKED);
 	pthipth_yield();
+	return 0;
     }
-    else
+
+    pthipth_private_t *tmp = blocked_state.head;
+    while (tmp)
     {
-	pthipth_private_t *tmp = blocked_state.head;
-	while (tmp)
-	{
-	    pthipth_private_t *next_tmp = tmp->next;
-	    if (tmp->state == BLOCKED && tmp->current_barrier == barrier)
-		change_to_state(tmp, READY);
-	    tmp = next_tmp;
-	}
-	atomic_store(&barrier->waiting, 0);
-	return 1;
+	pthipth_private_t *next_tmp = tmp->next;
+	if (tmp->state == BLOCKED && tmp->current_barrier == barrier)
+	    change_to_state(tmp, READY);
+	tmp = next_tmp;
     }
-    return 0;
+    atomic_store(&barrier->waiting, 0);
+    return PTHIPTH_BARRIER_SERIAL_THREAD;
 }
--- pthipth_barrier.h
+++ pthipth_barrier.h
@@ -3,12 +3,14 @@
 
 #include <stdatomic.h>
 
+#define PTHIPTH_BARRIER_SERIAL_THREAD 1
+
 typedef struct pthipth_barrier {
     int count;
     atomic_int waiting;
 } pthipth_barrier_t;
 
-void pthipth_barrier_init(pthipth_barrier_t *barrier, int count);
+int pthipth_barrier_init(pthipth_barrier_t *barrier, int count);
 
 int pthipth_barrier_wait(pthipth_barrier_t *barrier);
 

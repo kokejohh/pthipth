0185ed9d9ebdb66ebf3b62849e5b68d1effdbd9e
Pitchakorn Thiprangsi <kokejohh@gmail.com>
2025-04-18 04:40:49 +0700

    feat: add block/unblock signal and not reset last_selected

--- src/pthipth_sleep.c
+++ src/pthipth_sleep.c
@@ -4,16 +4,21 @@
 #include "pthipth_prio.h"
 #include "pthipth_queue.h"
 #include "pthipth_avl.h"
+#include "pthipth_signal.h"
 
 extern pthipth_queue_t sleeping_state;
 
 void pthipth_sleep(uint64_t millisec)
 {
+    __PTHIPTH_SIGNAL_BLOCK();
+
     pthipth_private_t *self = __pthipth_selfptr();
 
     self->wake_time = __pthipth_gettime_ms() + millisec;
 
     __pthipth_change_to_state(self, SLEEPING);
+
+    __PTHIPTH_SIGNAL_UNBLOCK();
 
     pthipth_yield();
 }
@@ -30,10 +35,7 @@ void __pthipth_check_sleeping()
     {
 	pthipth_private_t *next_tmp = tmp->next;
 	if (tmp->state == SLEEPING && current_time >= tmp->wake_time)
-	{
-	    tmp->last_selected = current_time;
 	    __pthipth_change_to_state(tmp, READY);
-	}
 	tmp = next_tmp;
     }
 }

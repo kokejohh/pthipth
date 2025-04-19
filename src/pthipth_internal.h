f90d6fd23bef08a0f5ac3b031ef0b4911e9a94d1
Pitchakorn Thiprangsi <kokejohh@gmail.com>
2025-04-20 05:25:28 +0700

    refactor: public interface and internal interface

---
+++ src/pthipth_internal.h
@@ -0,0 +1,26 @@
+#ifndef PTHIPTH_INTERNAL_H
+#define PTHIPTH_INTERNAL_H
+
+#include "pthipth_internal_type.h"
+
+pid_t __pthipth_gettid();
+
+uint64_t __pthipth_gettime_ms();
+
+void __pthipth_set_thread_time_quota(int ms);
+
+void __pthipth_change_to_state(pthipth_private_t *node, pthipth_state_t to_state);
+
+pthipth_private_t *__pthipth_selfptr();
+
+int __pthipth_dispatcher(pthipth_private_t *node);
+
+void __pthipth_check_sleeping();
+
+void __pthipth_aging();
+
+void *__pthipth_idle(void *phony);
+
+int __pthipth_wrapper(void *arg);
+
+#endif
---
+++ src/pthipth_internal_type.h
@@ -0,0 +1,41 @@
+#ifndef PTHIPTH_INTERNAL_TYPE
+#define PTHIPTH_INTERNAL_TYPE
+
+#include <stdint.h>
+
+#include "futex.h"
+
+typedef struct pthipth_barrier pthipth_barrier_t;
+typedef struct pthipth_cond pthipth_cond_t;
+typedef struct pthipth_mutex pthipth_mutex_t;
+
+typedef enum pthipth_state {
+    RUNNING, READY, BLOCKED, SLEEPING, DEFUNCT
+} pthipth_state_t;
+
+// pthipth_private_t
+typedef struct pthipth_private {
+    pid_t tid;
+    pthipth_state_t state;
+    void *(*start_func)(void *);
+    void *arg;
+    void *return_value;
+    struct pthipth_private *blockedForJoin;
+    futex_t sched_futex;
+    uint8_t priority, init_priority, old_priority;
+    uint64_t wake_time;
+    uint64_t last_selected;
+    int time_quota;
+    int aging_factor;
+    int aging_time;
+    pthipth_mutex_t *current_mutex;
+    pthipth_cond_t *current_cond;
+    pthipth_barrier_t *current_barrier;
+    // Bucket queue and queue
+    struct pthipth_private *prev, *next, *inside_prev, *inside_next;
+    // AVL tree
+    struct pthipth_private *parent, *left, *right;
+    int height;
+} pthipth_private_t;
+
+#endif

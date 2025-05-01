7443a7460c2444b5b1b5b6e405ff80c416cbe0f3
Pitchakorn Thiprangsi <kokejohh@gmail.com>
2025-05-02 01:36:13 +0700

    feat: block signal in get/set

--- src/pthipth_get.c
+++ src/pthipth_get.c
@@ -1,10 +1,15 @@
 #include "pthipth_internal.h"
+#include "pthipth_signal.h"
 
 int pthipth_get_prio()
 {
+    __PTHIPTH_SIGNAL_BLOCK();
+
     pthipth_private_t *self = __pthipth_selfptr();
 
     if (self == NULL) return -1;
+
+    __PTHIPTH_SIGNAL_UNBLOCK();
 
     return self->priority;
 }
@@ -15,32 +20,46 @@ uint64_t pthipth_get_stack_size()
 
     if (self == NULL) return -1;
 
+    __PTHIPTH_SIGNAL_UNBLOCK();
+
     return self->stack_size;
 }
 
 int pthipth_get_time_quota()
 {
+    __PTHIPTH_SIGNAL_BLOCK();
+
     pthipth_private_t *self = __pthipth_selfptr();
 
     if (self == NULL) return -1;
+
+    __PTHIPTH_SIGNAL_UNBLOCK();
 
     return self->time_quota;
 }
 
 int pthipth_get_aging_factor()
 {
+    __PTHIPTH_SIGNAL_BLOCK();
+
     pthipth_private_t *self = __pthipth_selfptr();
 
     if (self == NULL) return -1;
+
+    __PTHIPTH_SIGNAL_UNBLOCK();
 
     return self->aging_factor;
 }
 
 int pthipth_get_aging_time()
 {
+    __PTHIPTH_SIGNAL_BLOCK();
+
     pthipth_private_t *self = __pthipth_selfptr();
 
     if (self == NULL) return -1;
+
+    __PTHIPTH_SIGNAL_UNBLOCK();
 
     return self->aging_time;
 }
--- src/pthipth_set.c
+++ src/pthipth_set.c
@@ -1,8 +1,11 @@
 #include "pthipth_internal.h"
 #include "pthipth_prio.h"
+#include "pthipth_signal.h"
 
 int pthipth_set_prio(int new_priority)
 {
+    __PTHIPTH_SIGNAL_BLOCK();
+
     pthipth_private_t *self = __pthipth_selfptr();
     
     if (self == NULL) return -1;
@@ -10,6 +13,8 @@ int pthipth_set_prio(int new_priority)
     self->priority = self->old_priority = new_priority;
 
     pthipth_prio_reinsert(self);
+
+    __PTHIPTH_SIGNAL_UNBLOCK();
 
     return 0;
 }

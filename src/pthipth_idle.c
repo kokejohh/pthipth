14d1c72e992ab42b10f56c642aa73ad78778e8bf
Pitchakorn Thiprangsi <kokejohh@gmail.com>
2025-05-09 02:04:11 +0700

    chore: delete line not use

--- src/pthipth_idle.c
+++ src/pthipth_idle.c
@@ -2,9 +2,6 @@
 #include "pthipth_queue.h"
 #include "pthipth_signal.h"
 
-extern pthipth_queue_t blocked_state;
-extern pthipth_queue_t sleeping_state;
-
 void *__pthipth_idle(void *phony)
 {
     (void)phony;

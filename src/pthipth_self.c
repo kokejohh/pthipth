62b1395c748cb2d0804f1d3a89329bdcc828a63d
Pitchakorn Thiprangsi <kokejohh@gmail.com>
2025-09-13 23:27:20 +0700

    feat: __pthipth_seltptr uses cur_pthipth instread avl search

--- src/pthipth_self.c
+++ src/pthipth_self.c
@@ -2,6 +2,8 @@
 #include "pthipth_internal.h"
 #include "pthipth_avl.h"
 
+extern __thread pthipth_private_t *cur_pthipth;
+
 pthipth_t pthipth_self()
 {
     return __pthipth_gettid();
@@ -9,5 +11,5 @@ pthipth_t pthipth_self()
 
 pthipth_private_t *__pthipth_selfptr()
 {
-    return pthipth_avl_search(__pthipth_gettid());
+    return cur_pthipth ? cur_pthipth : pthipth_avl_search(__pthipth_gettid());
 }
